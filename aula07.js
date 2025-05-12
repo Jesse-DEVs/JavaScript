f91d71ea8aef82d36b0948fa31404adae6e8b350
Jessé Miguel Ramos <jesse.miguel.2003@gmail.com>
2025-05-12 08:22:56 -0300

    atividades

---
+++ aula07.js
@@ -0,0 +1,38 @@
+//atividade 1
+
+const rosas = 8
+const lirio = 10
+const tulipa = 2
+
+let NumRosa = 50
+let NumLirio = 20
+let NumTulipa = 120
+
+let ValorRosa = rosas * NumRosa
+let ValorLirio = lirio * NumLirio
+let ValorTulipa = tulipa * NumTulipa
+
+let valorTotal = ValorRosa + ValorLirio + ValorTulipa
+
+console.log("Preço da Rosa:",rosas,"Quantidade:",NumRosa,"Valor total das vendas:",ValorRosa)
+console.log("Preço do Lirio:",lirio,"Quantidade:",NumLirio,"Valor total das vendas:",ValorLirio)
+console.log("Preço da Tulipa:",tulipa,"Quantidade:",NumTulipa,"Valor total das vendas:",ValorTulipa)
+console.log("Valor total das vendas:",valorTotal)
+
+// atividade 2
+
+const nome1 = "Maxwell Wright"
+const nome2 = "Raja Villarreal"
+const nome3 = "Helen Richards"
+
+let num1 = "0191 719 6495"
+let num2 = "0866 398 2895"
+let num3 = "0800 1111"
+
+let gmail1 = "Curabitur.egestas.nunc@nonuummyac.co.uk" 
+let gmail2 = "posuere.valputate@sed.com"
+let gmail3 = "libero@convallis.edu"
+
+console.log("Nome:",nome1,"Numero:",num1,"Gmail:",gmail1)
+console.log("Nome:",nome2,"Numero:",num2,"Gmail:",gmail2)
+console.log("Nome:",nome3,"Numero:",num3,"Gmail:",gmail3)
