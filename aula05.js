04ee2b56d6a6a667d2742ef4f52fd01d0b3e58ee
Jessé Miguel Ramos <jesse.miguel.2003@gmail.com>
2025-05-12 07:33:10 -0300

    aula05

--- aula05.js
+++ aula05.js
@@ -1,10 +1,18 @@
 
 
-let nome 
-nome = "jesse Miguel Ramos"
+const nome = "jesse Miguel Ramos"
+let idade = 22
 
-console.log("meu nome é:",nome)
+console.log("meu nome é:",nome,"e tenho",idade,"anos.")
 
+// O que acontece se declararmos algo usando let ou const dentro de um bloco? Isso criará uma variável ou constante local. Ela será visível apenas dentro do bloco em que foi declarada e em blocos que podem ser aninhados nele.
 
-
+let  height  =  180;
+{
+         let  weight  =  70;
+         console.log(height);  //  ->  180
+         console.log(weight);  //  ->  70	
+}
+console.log(height);  //  ->  180
+console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
 
