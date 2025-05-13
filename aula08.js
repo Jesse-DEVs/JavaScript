9473206f28c0fb7bcbfb9a0666529030d566b1ff
Jessé Miguel Ramos <jesse.miguel.2003@gmail.com>
2025-05-13 10:04:58 -0300

    aula08

---
+++ aula08.js
@@ -0,0 +1,15 @@
+//operadores de logicos
+//&& = END - 
+//|| = OR
+// ! = NOT
+
+// OPERAÇÃO TERNARIA 
+let status = "I"
+ //                    True : False
+res=(status =="A" ? "ATIVO": "INATIVO")
+console.log(res)
+
+//0 = False
+//1 = True
+
+// teste logico ? se verdadeiro : se falso
