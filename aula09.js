f327979b306809f996170b2308b63c3209ee0fda
Jessé Miguel Ramos <jesse.miguel.2003@gmail.com>
2025-05-13 11:01:02 -0300

    aula09

---
+++ aula09.js
@@ -0,0 +1,18 @@
+// Aula de TAPYOF
+
+let v1=10
+let v2="10"
+let v3=1.90
+let v4=true
+let v5=v1===v2
+let v6={nome:"jesse"}
+
+console.log("Valor:"+ v1 + " - Tipo: "+ typeof(v1))
+console.log("Valor:"+ v2 + " - Tipo: "+ typeof(v2))
+console.log("Valor:"+ v3 + " - Tipo: "+ typeof(v3))
+console.log("Valor:"+ v4 + " - Tipo: "+ typeof(v4))
+console.log("Valor:"+ v5 + " - Tipo: "+ typeof(v5))
+console.log("Valor:"+ v6 + " - Tipo: "+ typeof(v6))
+
+let x = v1+v2 
+console.log(x)
